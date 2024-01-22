a22e140223f523d126f6243091af40a1a59a2a20
nncen <igorgettka@gmail.com>
2024-01-22 15:44:40 +0100

    modified:   src/commands/admin/hash.js

--- src/commands/admin/hash.js
+++ src/commands/admin/hash.js
@@ -13,20 +13,35 @@ module.exports = {
     options: [
         {
             name: 'string',
-            description: 'Użytkownik do zbanowania',
+            description: '?',
             type: ApplicationCommandOptionType.String,
             required: true
         }
     ],
-    callback: ({interaction, channel}) => {
+    callback: ({interaction}) => {
 
         function getRandomInt(min, max) {
             min = Math.ceil(min);
             max = Math.floor(max);
             return Math.floor(Math.random() * (max - min)) + min;
         }
-
-        return createHash('sha256').update(interaction.options.getString("string")).update(createHash("sha256").update(toString(getRandomInt(0,10000), 'utf-8')).digest('hex')).digest('hex')
+        function makeid(length) {
+            let result = '';
+            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            const charactersLength = characters.length;
+            let counter = 0;
+            while (counter < length) {
+              result += characters.charAt(Math.floor(Math.random() * charactersLength));
+              counter += 1;
+            }
+            return result;
+        }
+        return createHash('sha256')
+        .update(interaction.options.getString("string"))
+        .update(createHash("sha256")
+        .update(makeid(getRandomInt(3,12), 'utf-8'))
+        .digest('hex'))
+        .digest('hex')
 
     },
 }
